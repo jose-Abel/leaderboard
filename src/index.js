7c274f6434558e53ffd35b3b548bd3378196f41e
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-10-06 12:35:08 -0400

    Create the handlePostsScores function and added to the submit button in a click event listener

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import './style.css';
 
-const refreshButton = document.getElementById('refresh-button');
+const gameID = 'Qh5qvu9aTOutMmVnTpfJ';
+const URL = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores`;
 
-const ulElement = document.querySelector('.recent-scores-ul');
+const ulElement = document.getElementById('recent-scores-ul');
+const refreshButton = document.getElementById('refresh-button');
+const nameInput = document.getElementById('form-name');
+const scoreInput = document.getElementById('form-score');
+const submitButton = document.getElementById('submit-button');
 
 const transformData = (data) => {
-    const {result} = data;
-    const [user, score] = result;
+  ulElement.innerHTML = '';
+
+  data.forEach((result) => {
+    const { user, score } = result;
 
     const liElement = document.createElement('li');
     const spanNameElement = document.createElement('span');
@@ -16,20 +23,55 @@ const transformData = (data) => {
     spanNameElement.classList.add('name');
     spanScoreElement.classList.add('score');
 
-    spanNameElement.innerHTML = `${user}: `
+    spanNameElement.innerHTML = `${user}: `;
     spanScoreElement.innerHTML = score;
 
     liElement.appendChild(spanNameElement);
     liElement.appendChild(spanScoreElement);
     ulElement.appendChild(liElement);
-}
+  });
+};
 
 const handleGetGames = async () => {
-    const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/A1daCs599tPxi2IG5de0/scores');
+  try {
+    const response = await fetch(URL);
 
     const data = await response.json();
 
-    transformData(data);
-}
+    const { result } = data;
+
+    transformData(result);
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const handlePostScores = async () => {
+  if (nameInput.value && scoreInput.value) {
+    try {
+      await fetch(URL, {
+        method: 'POST',
+        body: JSON.stringify({
+          user: nameInput.value,
+          score: scoreInput.value,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      });
+
+      nameInput.value = '';
+      scoreInput.value = '';
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+};
 
 refreshButton.addEventListener('click', handleGetGames);
+
+submitButton.addEventListener('click', handlePostScores);
+
+document.addEventListener('DOMContentLoaded', () => {
+  handleGetGames();
+});
